68e450f4e7efbe61a467e2e829d1ac50177eb4e6
Marcos Melo <marcos@marcosmelo.tech>
2022-01-27 16:47:11 -0500

    wip: creating card component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { reactive, onMounted } from 'vue'
+import MovieCard from "./components/MovieCard.vue";
+
+const state = reactive({ movies: [] })
+
+onMounted(() => {
+  fetch('https://hackeryou-marcos-api.herokuapp.com/movies')
+      .then(response => response.json())
+      .then(data => state.movies = data)
+})
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
-    Hello world!
+    <MovieCard v-for="movie in state.movies" :title="movie.title" :poster="movie.poster_path" :overview="movie.overview"/>
   </h1>
 
 </template>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- component -->
+  <div class=" rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3 md:mx-0 lg:mx-0">
+    <div class="w-full flex justify-between p-3">
+      <div class="flex">
+        <span class="pt-1 ml-2 font-bold text-sm">{{ props.title }}</span>
+      </div>
+      <span class="px-2 hover:bg-gray-300 cursor-pointer rounded"><i class="fas fa-ellipsis-h pt-2 text-lg"></i></span>
+    </div>
+    <img class="w-full bg-cover" :src=props.poster alt="Movie poster">
+    <div class="px-3 pb-2">
+      <div class="pt-1">
+        <div class="mb-2 text-sm">{{ props.overview }}</div>
+      </div>
+      <div class="mb-2">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+let props = defineProps({
+  title: String,
+  releaseDate: Number,
+  overview: String,
+  poster: String,
+  rate: Number
+})
+
+</script>
+
+<style scoped>
+
+</style>
